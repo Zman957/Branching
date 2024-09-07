e33cebc3889e3818b0f1976d5a06f3e1f650c7b0
Zubair Khan <zkhan15@mail.valenciacollege.edu>
2024-09-06 22:44:39 -0400

    initial commit

---
+++ Branching.py
@@ -0,0 +1,37 @@
+hours = 0.07633 * 1000
+second_hours = 0.09259 * 500
+third_hours = 0.0925 * 215
+first_thousand = 0.07633
+after_thousand = 0.09259
+
+kw_hours = 1500
+
+if(kw_hours > hours):
+    print("Amount owed is", hours + second_hours)
+else:
+    print("not right")
+
+kw_hours2 = 764
+
+if(kw_hours2 < hours):
+    print("Amount owed is",hours + second_hours)
+elif(kw_hours2 > hours):
+    print("Amount owed is",kw_hours2 * first_thousand)
+else:
+    print("Mi Bomoboooo")
+
+kw_hours3 = 1215
+
+if(kw_hours3 > hours):
+    print("Amount owed is",hours + third_hours)
+else:
+    print("I love fortnite")
+
+kw_hours4 = 812
+
+if(kw_hours4 < hours):
+    print:("Amount owed is",hours + second_hours)
+elif(kw_hours4 > hours):
+    print("Amount owed is",kw_hours4 * first_thousand)
+
+
